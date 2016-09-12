dec36ffce1fa10aa64954485e638f2ce4d83d063
KarthikaChinnasamy <karthikakcs21@gmail.com>
2016-09-12 18:17:32 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class reverse
+{
+public static void main( String args[])
+{
+String a;
+StringBuffer a=new StringBuffer("karthika");
+String s;
+s=a.reverse();
+System.out.println(s);
+}
